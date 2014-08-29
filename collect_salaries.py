15dbd1347a636afa46b88402bd18f900239f71f4
edz504 <eddiedzhou@gmail.com>
2014-08-28 19:59:04 -0700

    works up to spurs 2011

--- collect_salaries.py
+++ collect_salaries.py
@@ -52,8 +52,43 @@ def extractSalaryPercents(url):
     sal_percs = [sal / tot for sal in sals]
     return sal_percs
 
-for year in years[7:]:
+base = 'http://data.shamsports.com/content/pages/data/salaries/'
+for year in df.columns:
     url = base + str(year) + '/sixers.jsp'
     print extractSalaryPercents(url)
 
 
+###################
+# columns: team | year | player 1 salary percentage | player 2 salary percentage ...
+# say that maximum of 40 players are being paid (should be ~ 12)
+ncol = 42
+sal_perc_df = pd.DataFrame(np.ones((df.values[df.values == 1].size, ncol), 
+    dtype=np.float64), columns=['team', 'year'] + ['p' + str(i) for i in range(1,41)])
+
+# fill the salary df
+rc = 0
+scrape_bad = False
+for i in range(0, df.shape[0]):
+    for j in range(0, df.shape[1]):
+        if df.iloc[i, j] == 1:
+            sal_perc_df['team'][rc] = df.index[i]
+            sal_perc_df['year'][rc] = df.columns[j]
+            # scrape salary data
+            url = base + str(df.columns[j]) + '/' + df.index[i] + '.jsp'
+            try:
+                sal_percs = extractSalaryPercents(url)
+            except:
+                print 'Scraping error on url <' + url + '>'
+                print 'Improve your extract function, you noob.'
+                scrape_bad = True
+                break
+            sal_percs += [0] * ((ncol - 2) - len(sal_percs)) # pad the zeros
+            sal_perc_df.iloc[rc, 2:] = sal_percs # fill that row in         
+            rc += 1
+            print 'Finished payroll for ' + df.index[i] + ', ' + str(df.columns[j])
+        else:
+            print 'Skipping 404 page'
+    if scrape_bad:
+        break   
+
+# pickle.dump([df, sal_perc_df], open('payroll_shamsports.p', 'wb'))
