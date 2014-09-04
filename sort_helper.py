8787cf420f0aab0e98e69dcd35436c85d92ed7ab
edz504 <eddiedzhou@gmail.com>
2014-09-03 17:33:12 -0700

    f5 function from sorting link (preserve order)

---
+++ sort_helper.py
@@ -0,0 +1,17 @@
+# preserves order, from 
+# http://www.peterbe.com/plog/uniqifiers-benchmark
+def uniquify(seq, idfun=None): 
+   # order preserving
+   if idfun is None:
+       def idfun(x): return x
+   seen = {}
+   result = []
+   for item in seq:
+       marker = idfun(item)
+       # in old Python versions:
+       # if seen.has_key(marker)
+       # but in new ones:
+       if marker in seen: continue
+       seen[marker] = 1
+       result.append(item)
+   return result
