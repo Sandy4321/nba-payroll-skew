3ce662573a307696dd9bdfb61a2d35a401d8406a
edz504 <eddiedzhou@gmail.com>
2015-01-30 19:54:28 -0500

    updated nb for skew pictures and corr tables

--- vis_skews.R
+++ vis_skews.R
@@ -3,7 +3,10 @@ librar(reshape2)
 library(dplyr)
 
 perf_skew <- read.csv("skew_record.csv")
-ggplot(perf_skew, aes(x=skew, y=reg_season_wp)) + geom_point()
+ggplot(perf_skew, aes(x=skew, y=reg_season_wp)) + 
+    geom_point() + 
+    geom_smooth(method=lm, se=FALSE)
+ggsave(file="reg_season_by_skew.png", width=10, height=6)
 
 perf_skew_clean <- perf_skew[which(!is.na(perf_skew$skew)),]
 cor.test(perf_skew_clean$skew, perf_skew_clean$reg_season_wp)
@@ -11,8 +14,12 @@ cor.test(perf_skew_clean$skew, perf_skew_clean$reg_season_wp,
     method="kendall")
 cor.test(perf_skew_clean$skew, perf_skew_clean$reg_season_wp, 
     method="spearman")
+    
+ggplot(perf_skew, aes(x=skew, y=playoff_wp)) + 
+    geom_point() + 
+    geom_smooth(method=lm, se=FALSE)
+ggsave(file="playoff_by_skew.png", width=10, height=6)
 
-ggplot(perf_skew, aes(x=skew, y=playoff_wp)) + geom_point()
 perf_skew_clean_playoff <- perf_skew[which(!is.na(perf_skew_clean$playoff_wp)),]
 cor.test(perf_skew_clean_playoff$skew, perf_skew_clean_playoff$reg_season_wp)
 cor.test(perf_skew_clean_playoff$skew, perf_skew_clean_playoff$reg_season_wp, 
